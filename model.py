d53a74f89a0aa157e616cc998b11b82267931ff4
vagrant <vagrant@vagrant.vm>
2017-05-18 22:39:02 +0000

    afternoon brk/late lunch re finished user, user routes,fix login

--- model.py
+++ model.py
@@ -12,24 +12,25 @@ class User(db.Model):
 
     __tablename__ ="users"
 
-    user_id = db.Column(db.Integer, primary_key=True, unique=True)
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255))
+    password = db.Column(db.String(255), nullable=True)
 
     # Information
     fname = db.Column(db.String(50), nullable=False)
     lname = db.Column(db.String(50), nullable=False)
-    job_title = db.Column(db.String(100))
-    department = db.Column(db.String(50)) # if staff
-    birth_date = db.Column(db.Date()) # if staff
+    job_title = db.Column(db.String(100), nullable=True)
+    department = db.Column(db.String(50), nullable=True) 
+    birth_date = db.Column(db.Date(), nullable=True)
     phone = db.Column(db.String(30), nullable=False)
-    phone2 = db.Column(db.String(30))
-    company = db.Column(db.String(40))
+    phone2 = db.Column(db.String(30), nullable=True)
+    company = db.Column(db.String(40), nullable=True)
     # Address
-    address1 = db.Column(db.String(255))
-    address2 = db.Column(db.String(255))
-    city = db.Column(db.String(100))
-    state = db.Column(db.String(20))
+    address1 = db.Column(db.String(255), nullable=False)
+    address2 = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+    zipcode = db.Column(db.String(10), nullable=False)
 
     # Account status
     active = db.Column(db.Boolean())
@@ -39,9 +40,9 @@ class User(db.Model):
     roles = db.relationship('Role_ID', secondary='user_roles',
                             backref=db.backref('users', lazy='dynamic'))
 
-
+#corrected dunder repr 5/17/17
     def __repr__(self):
-        return '<User %r>' % (self.lname, self.fname)
+        return '<User user_id=%s>' % (self.user_id, self.email)
 
 
 class Role_ID(db.Model):
@@ -70,9 +71,9 @@ class Product(db.Model):
 
     __tablename__ ="product"
 
-    product_id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.Unicode(50), nullable=False, unique=True)
-    product_number = db.Column(db.String(10))
+    product_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_name = db.Column(db.Unicode(50), nullable=True)
+    product_number = db.Column(db.String(10), nullable=False, unique=True)
     description = db.Column(db.Unicode(255))
     product_type = db.Column(db.Unicode(100))
     price = db.Column(db.DECIMAL(10, 2))
@@ -99,7 +100,7 @@ class Job(db.Model):
     __tablename__ = 'jobs'
 
     job_id = db.Column(db.Integer, primary_key=True)
-    product_quantity = db.Column(db.Integer)
+    product_quantity = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     customer_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     total = db.Column(db.Float(precision=10, decimal_return_scale=2, asdecimal=True))
@@ -160,5 +161,6 @@ def connect_to_db(app):
 if __name__ == "__main__":
 
     from server import app
+    
     connect_to_db(app)
     print "Connected to DB."
--- server.py
+++ server.py
@@ -1,25 +1,26 @@
-
+import os
 import json
 import datetime
-import os
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 from flask import Flask, jsonify, render_template, request, flash, redirect, url_for
-# from model import connect_to_db, db, Customer, Invoice, Product, Invoice_Detail, Role_ID, User 
+from model import connect_to_db, db, Job, Product, User, Role_ID, Job_Product 
 from flask_debugtoolbar import DebugToolbarExtension
-
+#app.jinja_env.undefined = StrictUndefined
 
 app = Flask(__name__)
-#app.jinja_env.undefined = StrictUndefined
+
 app.secret_key = "ABC"
 
 @app.errorhandler(404)
 def page_not_found(error):
+
     return render_template('page_not_found.html'), 404
 
 @app.route('/')
 def index():
     """Homepage."""
+
     return render_template("homepage.html")
 
 @app.route('/search')
@@ -48,6 +49,7 @@ def login():
 
     if not user:
         flash('User not found!')
+        return redirect('/login')
     elif password == str(user.password):
         session['user_id'] = user.user_id
         flash('User: {} has been logged in!'.format(email))
@@ -64,12 +66,22 @@ def logout():
 
     del session['user_id']
     flash('User has been logged out')
-
+    #?return redirect("/") or ?
     return render_template('homepage.html')
+
+
+@app.route('/users/<int:user_id>')
+def user_detail(user_id):
+        """Show User Information"""
+
+        user = User.query.get(user_id)
+        return render_template("show_user.html", user=user)
+
 
 @app.route('/dashboard')
 def dashboard():
     """Dashboard"""
+
     user_id = session.get('user_id')
     user = User.query.filter_by(user_id=user_id).first()
     role = user.roles.name
@@ -80,148 +92,157 @@ def dashboard():
     #   find quotes, projects, invoices
     #   show the customer dash, pass in data
     # ... same for estimator, staff
-
-
 #     return render_template('templates/dashboard.html', page=page)
 
 
-@app.route('/add_customer', methods=["GET", "POST"])
-def add_customer():
-    """Display Add Customer Form"""
+@app.route('/users')
+def user_list():
+    """Show List of All Users is ECRM."""
+
+    user = User.query.get(user_id)
+    return render_template("all_users.html", user=user)
+
+
+
+
+# @app.route('/add_customer', methods=["GET", "POST"])
+# def add_customer():
+#     """Display Add Customer Form"""
     
-    return render_template("templates/create_new_user.html")
+#     return render_template("templates/create_new_user.html")
 
 
-@app.route('/process_add_customer', methods=["GET", "POST"])
-def add_customer_to_db():
-    """Add customer to DB."""
-    page = 'create_new_user'
-    fname = request.args.get('fname')
-    lname = request.args.get('lname')
-    zip_code = request.args.get('zipcode')
-    email = request.args.get('email')
-    created_date = datetime.datetime.strptime(
-            str(request.get['datetime']),
-            '%m/%d/%Y'
-        ).strftime('%Y-%m-%d')
-    password = request.args.get('password')
-    phone = request.args.get('phone')
-    phone2 = request.args.get('phone2')
-    address1 = request.args.get('address1')
-    address12= request.args.get('address2')
-    city = request.args.get('city')
-    state = request.args.get('state')
-    company = request.args.get('company')
+# @app.route('/process_add_customer', methods=["GET", "POST"])
+# def add_customer_to_db():
+#     """Add customer to DB."""
+#     page = 'create_new_user'
+#     fname = request.args.get('fname')
+#     lname = request.args.get('lname')
+#     zip_code = request.args.get('zipcode')
+#     email = request.args.get('email')
+#     created_date = datetime.datetime.strptime(
+#             str(request.get['datetime']),
+#             '%m/%d/%Y'
+#         ).strftime('%Y-%m-%d')
+#     password = request.args.get('password')
+#     phone = request.args.get('phone')
+#     phone2 = request.args.get('phone2')
+#     address1 = request.args.get('address1')
+#     address12= request.args.get('address2')
+#     city = request.args.get('city')
+#     state = request.args.get('state')
+#     company = request.args.get('company')
   
 
-    customer = Customer(fname=fname, lname=lname, zipcode=zip_code, email=email,
-                        password=password, phone=phone, phone2=phone2, 
-                        address1=address1, address2=address2, city=city, state=state,
-                        company=company)
+#     customer = Customer(fname=fname, lname=lname, zipcode=zip_code, email=email,
+#                         password=password, phone=phone, phone2=phone2, 
+#                         address1=address1, address2=address2, city=city, state=state,
+#                         company=company)
    
-    db.session.add(customer)
+#     db.session.add(customer)
    
-    db.session.commit()
+#     db.session.commit()
     
-    flash("Customer was addded successfully!!!")
+#     flash("Customer was addded successfully!!!")
 
-    return redirect(url_for('/')) #DASHBOARD??????
+#     return redirect(url_for('/')) #DASHBOARD??????
     
-    return render_template('create_new_user.html', page=page)
+#     return render_template('create_new_user.html', page=page)
 
-@app.route('/create_new_user', methods=["GET", "POST"])
-def new_user():
-    page = 'create_new_user'
-    if request.method == "POST":
-        firstname = request.form['first_name']
-        lastname = request.form['last_name']
-        f_name = request.args.get('fname')
-        l_name = request.args.get('lname')
-        zip_code = request.args.get('zipcode')
-        email = request.args.get('email')
-        created_date = datetime.datetime.strptime(
-            str(request.get['datetime']),
-            '%m/%d/%Y'
-        ).strftime('%Y-%m-%d')
-        password = request.args.get('password')
-        phone = request.args.get('phone')
-        phone2 = request.args.get('phone2')
-        address1 = request.args.get('address1')
-        address12= request.args.get('address2')
-        city = request.args.get('city')
-        state = request.args.get('state')
-        #IF STAFF add these two ASK LESLIE HOW ORGANIZE
-        # job_title = request.form['job_title']
-        # department = request.form['department']
+# @app.route('/create_new_user', methods=["GET", "POST"])
+# def new_user():
+#     page = 'create_new_user'
+#     if request.method == "POST":
+#         firstname = request.form['first_name']
+#         lastname = request.form['last_name']
+#         f_name = request.args.get('fname')
+#         l_name = request.args.get('lname')
+#         zip_code = request.args.get('zipcode')
+#         email = request.args.get('email')
+#         created_date = datetime.datetime.strptime(
+#             str(request.get['datetime']),
+#             '%m/%d/%Y'
+#         ).strftime('%Y-%m-%d')
+#         password = request.args.get('password')
+#         phone = request.args.get('phone')
+#         phone2 = request.args.get('phone2')
+#         address1 = request.args.get('address1')
+#         address12= request.args.get('address2')
+#         city = request.args.get('city')
+#         state = request.args.get('state')
+#         #IF STAFF add these two ASK LESLIE HOW ORGANIZE
+#         # job_title = request.form['job_title']
+#         # department = request.form['department']
   
-        user = User(firstname=first_name, lastname=last_name, f_name=fname, l_name=lname, zipcode=zip_code, email=email, created_date=datetime,
-                        password=password, phone=phone, phone2=phone2, 
-                        address1=address1, address2=address2, city=city, state=state)
+#         user = User(firstname=first_name, lastname=last_name, f_name=fname, l_name=lname, zipcode=zip_code, email=email, created_date=datetime,
+#                         password=password, phone=phone, phone2=phone2, 
+#                         address1=address1, address2=address2, city=city, state=state)
 
-        db.session.add(user)
+#         db.session.add(user)
    
-        db.session.commit()
+#         db.session.commit()
   
-        flash("User was addded successfully!!!")
+#         flash("User was addded successfully!!!")
         
-        return redirect(url_for('/'))
+#         return redirect(url_for('/'))
 
-    else:
-        return render_template('create_new_user.html', page=page)
+#     else:
+#         return render_template('create_new_user.html', page=page)
 
-@app.route('/customers', methods=["POST"])
-def show_customer():
-    page='show_customer'
-    if request.method == "POST":
+# @app.route('/customers', methods=["POST"])
+# def show_customer():
+#     page='show_customer'
+#     if request.method == "POST":
 
 
-        @app.route('/Create_Invoice/', methods = ['GET', 'POST'])
-        def create_invoice():
-            task = raw_input("Enter 'n' to create new invoice, press 'q' to exit: ")
+    #     @app.route('/Create_Invoice/', methods = ['GET', 'POST'])
+    #     def create_invoice():
+    #         task = raw_input("Enter 'n' to create new invoice, press 'q' to exit: ")
 
-        if (task == 'n'):
-            product_list = []
-            quantity = []
-            product_price = []
+    #     if (task == 'n'):
+    #         product_list = []
+    #         quantity = []
+    #         product_price = []
     
-    while True:
-        product_number = raw_input("\nEnter the 8 digit item code. Or press 'q' " + 
-                         "to quit: ")
-        if (product_number == 'q'):
-            print("\nQuitting ...\n")
-            break
-        if (len(product_number) != 8): 
-            print("\nInvalid product number, please try again.")
-        else:
-            with open("product_list.txt") as products:
-                for line in products:
-                    if product_number in line:
-                        single_product = line.split(" ")
-                        quantity = input("\nQuantity of the product " +
-                                         "to be purchased: ")
-                        code = single_product[0]
-                        product_name = single_product[1]
-                        price = float(single_product[2])
-                        total = (price) * int(quantity)
-                        product_list.append(product_name)
-                        quantity.append(quantity)
-                        product_price.append(total)
-                        break
-    print ("Your invoice: ")
-    for i in range(len(product_list)):
-        print ('\single_product', quantity[i], product_list[i],' for the ' +
-              'amount of $', product_price[i])
-    print ("Your total: $ ", sum(product_price))                   
-    if (task == "q"):
-        sys.exit()
+    # while True:
+    #     product_number = raw_input("\nEnter the 8 digit item code. Or press 'q' " + 
+    #                      "to quit: ")
+    #     if (product_number == 'q'):
+    #         print("\nQuitting ...\n")
+    #         break
+    #     if (len(product_number) != 8): 
+    #         print("\nInvalid product number, please try again.")
+    #     else:
+    #         with open("product_list.txt") as products:
+    #             for line in products:
+    #                 if product_number in line:
+    #                     single_product = line.split(" ")
+    #                     quantity = input("\nQuantity of the product " +
+    #                                      "to be purchased: ")
+    #                     code = single_product[0]
+    #                     product_name = single_product[1]
+    #                     price = float(single_product[2])
+    #                     total = (price) * int(quantity)
+    #                     product_list.append(product_name)
+    #                     quantity.append(quantity)
+    #                     product_price.append(total)
+    #                     break
+    # print ("Your invoice: ")
+    # for i in range(len(product_list)):
+    #     print ('\single_product', quantity[i], product_list[i],' for the ' +
+    #           'amount of $', product_price[i])
+    # print ("Your total: $ ", sum(product_price))                   
+    # if (task == "q"):
+    #     sys.exit()
 
-        db.session.add(invoice)
-        db.session.commit()
-        return redirect(url_for('show_invoice'))
+    #     db.session.add(invoice)
+    #     db.session.commit()
+    #     return redirect(url_for('show_invoice'))
 
-    return render_template("create_invoice.html", title = gettext('create_invoice'), form = form)
+    # return render_template("create_invoice.html", title = gettext('create_invoice'), form = form)
 
 app.jinja_env.globals.update(Create_Invoice="create_invoice.html")
+
 @app.route('/invoice/confirm/<int:invoice_id>')
 def confirm_invoice(invoice_id):
     invoice = Invoice.query.filter(Invoice.id==invoice_id).first()
@@ -281,12 +302,8 @@ def invoice_delete(invoice_id):
 
 if __name__ == "__main__":
     app.debug = True
-
     app.jinja_env.auto_reload = app.debug  
-
     connect_to_db(app)
-
     DebugToolbarExtension(app)
-
     app.run(port=5001, host='0.0.0.0')
 
